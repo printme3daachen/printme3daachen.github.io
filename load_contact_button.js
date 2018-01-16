28f4900ae51d655f5f04a0cee8449eff51cd75be
Maxi Fries <Maxi.Fries97@gmail.com>
2018-01-16 22:46:30 +0100

    init

---
+++ load_contact_button.js
@@ -0,0 +1,10 @@
+document.write('\
+    <link rel="stylesheet" type="text/css" href="contact.css">\
+    <div id= "button" class="mdl-color--accent mdl-color-text--accent-contrast mdl-shadow--16dp">KONTAKT\
+    \
+    <div id= "button_text_box" class="mdl-color-text--accent-contrast ">\
+    </div>\
+    <a href="mailto:3ddruck.aachen@gmail.com?subject=Anfrage 3D Druck&amp;body=Hallo%20Max,%0D%0A%0D%0Ahier%20steht%20die%20Nachricht." id= "button_fixed" class="mdl-js-button mdl-js-ripple-effect mdl-shadow--16dp">EMAIL</a>\
+    \
+    </div>\
+    ');
